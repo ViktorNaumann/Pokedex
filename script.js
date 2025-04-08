78ee3d37a067372165116a3b2a77116582571591
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-04-08 17:33:03 +0200

    loadMorePokemon

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ function init() {
 }
 
 async function getData() {
-  const spinner = document.getElementById("spinner");
+  const spinner = document.getElementById("spinner-overlay");
+
   spinner.classList.remove("hidden");
-  for (let i = 1; i <= 151; i++) {
+  for (let i = 1; i <= 20; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const response = await fetch(url);
     const data = await response.json();
@@ -33,5 +34,23 @@ function templateHtmlRenderPokemon(data) {
       </div>
     `;
   }
+
+  async function loadMorePokemon() {
+    const spinner = document.getElementById("spinner-overlay");
+    spinner.classList.remove("hidden");
+  
+    const startIndex = allPokemonData.length + 1;
+    const endIndex = startIndex + 19;
+  
+    for (let i = startIndex; i <= endIndex; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      createCard(data);
+      allPokemonData.push(data);
+    }
+    spinner.classList.add("hidden");
+  }
+  
 
  
