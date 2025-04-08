8220622e16be48b8682f3cfd7f23837d496afdbd
Viktor Naumann <Viktor_Naumann@hotmail.de>
2025-04-08 15:45:54 +0200

    background color - pokemon

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+let allPokemonData = [];
+
 function init() {
   getData();
 }
 
 async function getData() {
-  const pokemonList = document.getElementById('pokemon-list');
-
-  for (let i = 1; i <= 20; i++) {
+  const spinner = document.getElementById("spinner");
+  spinner.classList.remove("hidden");
+  for (let i = 1; i <= 151; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     const response = await fetch(url);
     const data = await response.json();
+    createCard(data);
+    allPokemonData.push(data);
+    console.log(data);
+  }
+  spinner.classList.add("hidden");
+}
 
+function createCard(data) {
+    const pokemonList = document.getElementById('pokemon-list');
     pokemonList.innerHTML += templateHtmlRenderPokemon(data);
   }
-}
-
+  
 function templateHtmlRenderPokemon(data) {
-  return /*html*/`
-    <div class="pokemon-card">
-      <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
-      <img src="${data.sprites.front_default}" alt="${data.name}">
-      <p><strong>#${data.id}</strong></p>
-      <p>Typ: ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-    </div>
-  `;
-}
+    return /*html*/`
+      <div class="pokemon-card bg_${data.types[0].type.name}">
+        <h2>${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</h2>
+        <img src="${data.sprites.front_default}">
+        <p><strong>#${data.id}</strong></p>
+        <p>Typ: ${data.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+      </div>
+    `;
+  }
 
+ 
